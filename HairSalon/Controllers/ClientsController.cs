00d24e8d914743108888895d965ce064ecf7b36a
Art Weinstein <artur.weinstein@gmail.com>
2021-10-08 09:37:27 -0700

    create models for Client, Stylist and HairSalonContext.

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Edit (int id)
+    {
+      
+    }
+  }
+}
